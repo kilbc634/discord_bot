feec7b5678609ae7258bbab2c907fa95ce69364f
kilbc634 <tuyn76801@gmail.com>
2020-08-08 17:11:17 +0800

    Add end point service for discord bot, and support discord DM message

--- discord_bot.py
+++ discord_bot.py
@@ -6,10 +6,59 @@ import traceback
 import os
 import time
 import subprocess
+import threading
+from flask import Flask, jsonify, request
+import json
+import asyncio
+
+client = None
+TOKEN = '***'
+endPoint = Flask('extraService')
 
 Affect_channels = ['bot乱交', '車']
+clannelId_onCar = 557487268580032512
+clannelId_onTest = 643265881996132362
+userId_owner = 407554740906360833
 
 class MyClient(discord.Client):
+    async def send_DM(self, userId, text):
+        channel = client.get_user(userId)
+        await channel.send(text)
+
+    async def forDM(self, message):
+        try:
+            print("--------------------- header ---------------------")
+            print(message)
+            print("--------------------- content ---------------------")
+            print(message.content)
+            print("--------------------- attachments -----------------")
+            print(message.attachments)
+        except:
+            print("!!! PRINT ERROR !!!")
+
+        if message.content == 'systemcall:logout' or message.content == 'SC:logout':
+            exit()
+
+        if message.author.id != userId_owner:
+            channel = client.get_user(userId_owner)
+            sender = message.author.name
+            msg = message.content
+            await channel.send('from {sender}:\n{msg}'.format(**locals()))
+
+        if message.content.find('!SAY') == 0:
+            text = message.content.split(' ', 1)[1]
+            channel = client.get_channel(clannelId_onTest)
+            await channel.send(text)
+        
+        if message.content.find('!MEMBER_LIST') == 0:
+            members = client.get_all_members()
+            members_list = list()
+            for member in members:
+                user_name = member.display_name
+                user_id = member.id
+                members_list.append('{user_name} ({user_id})'.format(**locals()))
+            await message.channel.send('\n'.join(members_list))
+    
     async def on_ready(self):
         print('Logged on as', self.user)
 
@@ -18,8 +67,13 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
         # specify a channel
-        if message.channel.name not in Affect_channels:
+        try:
+            if message.channel.name not in Affect_channels:
+                return
+        except AttributeError: # if message object type is <DMChannel>
+            await self.forDM(message)
             return
+            
         try:
             print("--------------------- header ---------------------")
             print(message)
@@ -30,7 +84,7 @@ class MyClient(discord.Client):
         except:
             print("!!! PRINT ERROR !!!")
         ### SystemCall zone ###
-        if message.content == 'systemcall:logout':
+        if message.content == 'systemcall:logout' or message.content == 'SC:logout':
             exit()
         ### function zone ###
         if message.content.find('!') == 0:
@@ -54,10 +108,34 @@ class MyClient(discord.Client):
         if message.content == 'tag_test':
             await message.channel.send('<@300498879433146389>') # @Matcha#8964
 
+def async_execute(func):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(func)
+
+@endPoint.route("/")
+def welcome():
+    return "This is End Point Service of Discord bot" , 200
+
+@endPoint.route("/message_user", methods=["POST"])
+def message_user():
+    data = request.json
+    sender = data['sender']
+    user = data['user']
+    message = data['message']
+    if user == 'me':
+        userId = userId_owner
+    else:
+        userId = int(user)
+    client.loop.create_task(client.send_DM(userId, 'from {sender}:\n{message}'.format(**locals())))
+    return 'OK', 200
+
 if __name__=='__main__':
     myDir = os.getcwd()
     p = subprocess.Popen('python ' + myDir + '\connect_server.py')
-    time.sleep(5)
+    time.sleep(3)
 
     client = MyClient()
-    client.run('NjA2NjY*****************************************90Y9MW8')
+    thread_discordBot = threading.Thread(target=client.run, args=(TOKEN,))
+    thread_discordBot.start()
+    
+    endPoint.run(host='0.0.0.0', debug=True, port=21090, use_reloader=False)
