025cc052398c58ab272c3629ffcd9ae7f83bf220
kilbc634 <tuyn76801@gmail.com>
2022-03-06 12:15:45 +0800

    Fix pick up cluster jewel tool

--- lib/poeNinjaModel.py
+++ lib/poeNinjaModel.py
@@ -10,21 +10,25 @@ def pickUp_ClusterJewel(apiUrl):
         if 'Cluster Jewel' in jewelData['itemData']['typeLine']:
             # get Jewel Type
             jewelType = jewelData['itemData']['typeLine']
-            for mod in jewelData['itemData']['enchantMods']:
-                if 'Added Small Passive Skills grant:' in mod:
-                    # get Enchant Mod
-                    enchantMod = mod
+            # get Enchant Mod. If item match unique rule, will set the special Enchant Mod
+            if jewelData['itemData']['name'] == 'Megalomaniac':
+                enchantMod = 'Unique Jewel: {}'format('Megalomaniac'.upper())
+            else:
+                for mod in jewelData['itemData']['enchantMods']:
+                    if 'Added Small Passive Skills grant:' in mod:
+                        # get Enchant Mod
+                        enchantMod = mod
             passiveSkillMods = list()
             for mod in jewelData['itemData']['explicitMods']:
                 if '1 Added Passive Skill is' in mod:
                     # get Passive Skill Mods
                     passiveSkillMods.append(mod)
-            jewelData = {
+            jewelDataOutput = {
                 'jewelType': jewelType,
                 'enchantMod': enchantMod,
                 'passiveSkillMods': passiveSkillMods
             }
-            pickUpDatas.append(jewelData)
+            pickUpDatas.append(jewelDataOutput)
 
     #print(json.dumps(pickUpDatas, sort_keys=True, indent=4))
     return pickUpDatas
